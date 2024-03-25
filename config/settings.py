8c0b5752b6d0775dce4a6d96e421fe790ea75199
meaume <meaume77@gmail.com>
2024-03-26 00:36:17 +0900

    finish

--- config/settings.py
+++ config/settings.py
@@ -138,9 +138,6 @@ DATABASES['default'].update(db_from_env)
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# The URL to use when referring to static files (where they will be served from)
-STATIC_URL = '/static/'
-
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
