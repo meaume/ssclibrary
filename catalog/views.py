b4f629f183a0c23b78dafdf0041147299dac50dc
meaume <meaume77@gmail.com>
2024-03-26 02:45:47 +0900

    ffinish

--- catalog/views.py
+++ catalog/views.py
@@ -7,7 +7,6 @@ def index(request):
     """View function for home page of site."""
 
     # Generate counts of some of the main objects
-    num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
 
     # Available books (status = 'a')
@@ -17,7 +16,6 @@ def index(request):
     num_authors = Author.objects.count()
 
     context = {
-        'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
